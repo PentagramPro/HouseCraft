3166800177d249a4eed96e7ce8162caea14fd5d4
PentagramPro <maxon-line@yandex.ru>
2014-10-09 00:17:16 +0400

    Finished with main menu

---
+++ Assets/Src/UI/LevelItemController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelItemController : MonoBehaviour {
+
+	Image image;
+	Color baseColor;
+	public Color selectedColor = new Color(0.6f,0.1f,0.1f);
+
+
+	public string SceneName;
+	public LevelSelectorController LevelSelector{get;internal set;}
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Button>().onClick.AddListener(OnClick);
+		image = GetComponent<Image>();
+		baseColor = image.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnClick()
+	{
+		LevelSelector.OnLevelSelected(this);
+		image.color = selectedColor;
+	}
+
+	public void Deselect()
+	{
+		image.color = baseColor;
+	}
+}
---
+++ Assets/Src/UI/LevelSelectorController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class LevelSelectorController : MonoBehaviour {
+
+	public List<LevelItemController> Levels = new List<LevelItemController>();
+	LevelItemController selectedLevel = null;
+	// Use this for initialization
+	void Start () {
+		foreach(LevelItemController level in Levels)
+			level.LevelSelector = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnLevelSelected(LevelItemController l)
+	{
+		selectedLevel = l;
+		foreach(LevelItemController level in Levels)
+		{
+			level.Deselect();
+		}
+	}
+
+	public void OnPlay()
+	{
+		if(selectedLevel==null)
+			return;
+	}
+}
